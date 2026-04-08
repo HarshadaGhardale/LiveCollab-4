bcc10fce2d98a37b262fa9a22f39aed2bfffb85c
HarshadaGhardale <hsgsunita@gmail.com>
2026-04-09 00:15:30 +0530

    fix render port

--- server/index.ts
+++ server/index.ts
@@ -97,8 +97,9 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5001", 10);
-  httpServer.listen(port, () => {
-    log(`serving on port ${port}`);
-  });
+ const port = Number(process.env.PORT);
+
+httpServer.listen(port, () => {
+  log(`serving on port ${port}`);
+});
 })();
